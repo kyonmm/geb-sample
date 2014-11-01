66909a0b22459a48394107b252f447cb453a31a6
kyon_mm <kyon.mm@gmail.com>
2014-11-01 15:19:05 +0900

    add spock reports

---
+++ src/test/groovy/org/kyonmm/Scenario.groovy
@@ -0,0 +1,12 @@
+package org.kyonmm
+
+import geb.spock.GebReportingSpec
+
+class Scenario extends GebReportingSpec {
+
+    // アプリケーションを使い始めるための設定をする
+    //
+    def "管理者としてログインしてチームメンバーの追加とログインを確認してもらう"(){
+
+    }
+}
